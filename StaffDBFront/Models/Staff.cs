1f02582bb6beb075c4fb3a8e70cce88c2e6bd8d9
Lamde23 <126534687+Lamde23@users.noreply.github.com>
2024-09-12 00:43:00 +1200

    Validations corrected for form

--- StaffDBFront/Models/Staff.cs
+++ StaffDBFront/Models/Staff.cs
@@ -5,16 +5,25 @@ namespace StaffDBFront.Models
 	public class Staff
 	{
 		public int Id { get; set; }
+
+		[Required(ErrorMessage = "First name is required")]
+		[MaxLength(50, ErrorMessage = "Max first name characters: 50")]
 		public string FirstName { get; set; }
+
+		[Required(ErrorMessage = "Last name is required")]
+		[MaxLength(50, ErrorMessage = "Maximum last name characters: 50")]
 		public string LastName { get; set; }
 
 		[Required(ErrorMessage = "Email is required")]
+		[MaxLength(50, ErrorMessage = "Max Email characters: 50")]
 		[EmailAddress(ErrorMessage = "Invalid Email Address")]
-		public string Email { get; set; } = string.Empty;
-		public DateTime EnrolledDate { get; set; }
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Enrolled date required")]
+		public DateTime EnrolledDate { get; set; } = DateTime.Today;
 		public bool Active { get; set; }
 
-		[RegularExpression(@"^\d{10}$|^$" , ErrorMessage = "Bank Account must be exactly 10 digits")]
+		[RegularExpression(@"^\d{10}$|^$" , ErrorMessage = "Bank Account must be exactly 10 digits or empty")]
 		public string? BankAccount { get; set; } = string.Empty;
 	}
 }
