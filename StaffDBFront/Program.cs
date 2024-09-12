b01c1b7868a874ee8df31cdccb031970428bc06f
Lamde23 <126534687+Lamde23@users.noreply.github.com>
2024-09-12 15:44:51 +1200

    IQueryable table added, sorting, pagination. Removed staff listing, updated form appearance.

--- StaffDBFront/Program.cs
+++ StaffDBFront/Program.cs
@@ -4,13 +4,12 @@ using Microsoft.Extensions.DependencyInjection;
 using StaffDBFront.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//direct db connection, used for scaffolding
 builder.Services.AddDbContextFactory<StaffDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("StaffDBContext") ?? throw new InvalidOperationException("Connection string 'StaffDBContext' not found.")));
 
-//builder.Services.AddHttpClient("staffBackEnd", client =>
-//{
-//	client.BaseAddress = new Uri("http://localhost:5263/"); //
-//});
+//base address for connection to back-end
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5263/") });
 
 builder.Services.AddQuickGridEntityFrameworkAdapter();
